c5bd5e4370fee570d4640972eb3cc66a28392202
jujulioed <julioeduardon9@hotmail.com>
2025-06-22 19:08:05 -0300

    Create Tasks table for the checklist
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -3,3 +3,14 @@ CREATE TABLE users (
     name VARCHAR(255),
     email VARCHAR(255)
 );
+
+CREATE TABLE tasks (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(127),
+    description VARCHAR(511),
+    user_id BIGINT NOT NULL,
+    done BOOL,
+    created_date DATE,
+    finished_at DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
